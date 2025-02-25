0e42fe11d8662f1e18b2f33c999f2a6b5e2ca08f
Leighton Grant <oneal.grant@outlook.com>
2025-02-25 19:55:18 +0000

    Integrate Material-UI theming and routing in main application entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import CssBaseline from '@mui/material/CssBaseline'
+import { BrowserRouter } from 'react-router'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+
+import './index.css'
+
+import '@fontsource-variable/quicksand'
+import { palletteDark, palletteLight } from './theme/pallette.js'
+import { typography } from './theme/typography.js'
+
+const theme = createTheme({
+	colorSchemes: {
+		dark: { palette: palletteDark },
+		light: { palette: palletteLight },
+	},
+	typography: typography,
+})
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<BrowserRouter basename='/stake-holder-register'>
+				<App />
+			</BrowserRouter>
+		</ThemeProvider>
+	</StrictMode>
 )
