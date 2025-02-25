70d0cfab443f239d8b92734cf506a0d861f9a8df
Leighton Grant <oneal.grant@outlook.com>
2025-02-25 19:56:26 +0000

    Add dark and light theme palettes and typography styles

---
+++ src/theme/pallette.js
@@ -0,0 +1,50 @@
+export const palletteDark = {
+	mode: 'dark',
+	common: {
+		black: 'hsl(0,0%,0%)',
+		white: 'hsl(0,0%,100%)',
+	},
+	primary: {
+		light: 'hsl(9,80%,70%)',
+		main: 'hsl(9,80%,65%)',
+		dark: 'hsl(9,80%,60%)',
+		contrastText: 'hsl(0,0%,100%)',
+	},
+	secondary: {
+		light: 'hsl(238, 27%, 50%)',
+		main: 'hsl(238, 27%, 45%)',
+		dark: 'hsl(238, 27%, 40%)',
+		contrastText: 'hsl(0,0%,100%)',
+	},
+	error: {
+		light: 'hsl(3, 61%, 60%)',
+		main: 'hsl(3, 61%, 50%)',
+		dark: 'hsl(3, 61%, 40%)',
+		contrastText: 'hsl(0,0%,100%)',
+	},
+}
+export const palletteLight = {
+	mode: 'light',
+	common: {
+		black: 'hsl(0,0%,0%)',
+		white: 'hsl(0, 0%, 100%)',
+	},
+	primary: {
+		light: 'hsl(9,80%,70%)',
+		main: 'hsl(9,80%,65%)',
+		dark: 'hsl(9,80%,60%)',
+		contrastText: 'hsl(0,0%,100%)',
+	},
+	secondary: {
+		light: 'hsl(238, 27%, 50%)',
+		main: 'hsl(238, 27%, 45%)',
+		dark: 'hsl(238, 27%, 40%)',
+		contrastText: 'hsl(0,0%,100%)',
+	},
+	error: {
+		light: 'hsl(3, 61%, 60%)',
+		main: 'hsl(3, 61%, 50%)',
+		dark: 'hsl(3, 61%, 40%)',
+		contrastText: 'hsl(0,0%,100%)',
+	},
+}
---
+++ src/theme/typography.js
@@ -0,0 +1,25 @@
+export const typography = {
+	fontFamily: ['Quicksand Variable', 'sans-serif'].join(','),
+
+	h1: {
+		fontSize: '3rem',
+		fontWeight: 600,
+		color: 'hsl(9,80%,65%)',
+	},
+	h3: {
+		fontSize: '2rem',
+		color: 'hsl(9,80%,65%)',
+	},
+	h4: {
+		fontSize: '1.5rem',
+		color: 'hsl(9,80%,65%)',
+	},
+	h5: {
+		fontSize: '1.25rem',
+		color: 'hsl(9,80%,65%)',
+	},
+	h6: {
+		fontSize: '1rem',
+		color: 'hsl(9,80%,65%)',
+	},
+}
