d1e9b57be0d1fc48c3daa9a92ab10f3dc97f3edf
Leighton Grant <oneal.grant@outlook.com>
2025-02-25 19:54:36 +0000

    Refactor App component to implement routing and session management with new layouts

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
-import './App.css'
+import MainLayout from './components/layouts/MainLayout'
+import AdminLayout from './components/layouts/AdminLayout'
+import Home from '@/components/pages/Home.jsx'
+import { Routes, Route } from 'react-router'
+import AdminApp from './components/AdminApp'
+import SignIn from './components/login/SignIn'
+import { useStore } from './lib/zustand'
 
 function App() {
+	const session = useStore(state => state.session)
+
+	if (!session) {
+		return (
+			<Routes>
+				<Route element={<AdminLayout />}>
+					<Route path='*' element={<SignIn />} />
+				</Route>
+			</Routes>
+		)
+	}
+
 	return (
-		<>
-			<h1>Stake Holder Register</h1>
-		</>
+		<Routes>
+			<Route element={<MainLayout />}>
+				<Route index element={<Home />} />
+			</Route>
+
+			<Route element={<AdminLayout />}>
+				<Route path='/admin/*' element={<AdminApp />} />
+			</Route>
+		</Routes>
 	)
 }
 
