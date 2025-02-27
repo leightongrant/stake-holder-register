26c8a8b46ed1c103522add09c208305c31a7fc68
Leighton Grant <oneal.grant@outlook.com>
2025-02-27 14:04:32 +0000

    Add sessionId state management to Zustand store for improved session handling

--- src/lib/zustand.js
+++ src/lib/zustand.js
@@ -3,14 +3,17 @@ import { account } from '../lib/appwrite'
 
 export const useStore = create(set => ({
 	user: JSON.parse(localStorage.getItem('appwrite_user')) || null,
+	sessionId: null,
 	session: account
 		.getSession('current')
 		.then(currentSession => {
 			set({ session: currentSession })
+			set({ isValid: currentSession.$id })
 		})
 		.catch(() => {
 			set({ session: null })
 		}),
 	setUser: current => set({ user: current }),
 	setSession: current => set({ session: current }),
+	setSessionId: current => set({ sessionId: current }),
 }))
