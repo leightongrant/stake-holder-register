4e522387682b13a8b647cf9bfa6636ad1b31c43d
Leighton Grant <oneal.grant@outlook.com>
2025-02-28 18:18:36 +0000

    Refactor StakeholderEdit component to update input fields for first name, last name, and job title, and remove unnecessary fields

--- src/components/StakeholderEdit.jsx
+++ src/components/StakeholderEdit.jsx
@@ -1,19 +1,16 @@
-import { DateInput, Edit, SimpleForm, TextInput } from 'react-admin'
+import { Edit, SimpleForm, TextInput } from 'react-admin'
 
 export const StakeholderEdit = () => (
 	<Edit>
 		<SimpleForm>
-			<TextInput source='id' />
-			<TextInput source='stakeholderName' />
+			<TextInput source='firstname' />
+			<TextInput source='lastname' />
 			<TextInput source='category' />
-			<TextInput source='stakeholderAnalysisGroup' />
+			<TextInput source='stakeholder_group' />
 			<TextInput source='organisation' />
-			<TextInput source='role' />
+			<TextInput source='job_title' />
 			<TextInput source='phone' />
 			<TextInput source='email' />
-			<TextInput source='address' />
-			<DateInput source='$createdAt' />
-			<DateInput source='$updatedAt' />
 		</SimpleForm>
 	</Edit>
 )
