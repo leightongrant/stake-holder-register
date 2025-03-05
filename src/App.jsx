428de6ef404dba993c20b459db04ecb2524bd415
Leighton Grant <oneal.grant@outlook.com>
2025-03-05 13:01:56 +0000

    Add user role check for admin access and redirect to Forbidden page

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,25 @@ import { Routes, Route } from 'react-router'
 import AdminApp from './components/AdminApp'
 import SignIn from './components/login/SignIn'
 import { useStore } from './lib/zustand'
+import Forbidden from './components/pages/Forbidden'
 
 function App() {
-	const { sessionId } = useStore()
+	const { sessionId, user } = useStore()
 
 	if (sessionId) {
 		return (
 			<Routes>
 				<Route element={<MainLayout />}>
-					<Route index element={<Home />} />
+					<Route path='*' element={<Home />} />
 				</Route>
 
 				<Route element={<AdminLayout />}>
-					<Route path='/admin/*' element={<AdminApp />} />
+					<Route
+						path='/admin/*'
+						element={
+							user?.labels.includes('admin') ? <AdminApp /> : <Forbidden />
+						}
+					/>
 				</Route>
 			</Routes>
 		)
