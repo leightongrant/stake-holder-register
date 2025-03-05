c37f544ac708fcaeb3f6a84a5e741fa141f36a66
Leighton Grant <oneal.grant@outlook.com>
2025-03-05 13:03:03 +0000

    Refactor FooterNavigation component to remove BottomNavigation and add copyright information

--- src/components/BottomNavigation.jsx
+++ src/components/BottomNavigation.jsx
@@ -1,26 +1,16 @@
+import { Typography, Link } from '@mui/material'
 import Box from '@mui/material/Box'
-import BottomNavigation from '@mui/material/BottomNavigation'
-import BottomNavigationAction from '@mui/material/BottomNavigationAction'
-import RestoreIcon from '@mui/icons-material/Restore'
-import FavoriteIcon from '@mui/icons-material/Favorite'
-import LocationOnIcon from '@mui/icons-material/LocationOn'
-import { useState } from 'react'
-
 const FooterNavigation = () => {
-	const [value, setValue] = useState(0)
 	return (
-		<Box component={'footer'}>
-			<BottomNavigation
-				showLabels
-				value={value}
-				onChange={(event, newValue) => {
-					setValue(newValue)
-				}}
-			>
-				<BottomNavigationAction label='Recents' icon={<RestoreIcon />} />
-				<BottomNavigationAction label='Favorites' icon={<FavoriteIcon />} />
-				<BottomNavigationAction label='Nearby' icon={<LocationOnIcon />} />
-			</BottomNavigation>
+		<Box component={'footer'} py={2}>
+			<Typography variant='body2' align='center'>
+				{'Copyright © '}
+				<Link color='inherit' href='https://mui.com/'>
+					Longleigh Foundation Stakeholder Register
+				</Link>{' '}
+				{new Date().getFullYear()}
+				{'.'}
+			</Typography>
 		</Box>
 	)
 }
