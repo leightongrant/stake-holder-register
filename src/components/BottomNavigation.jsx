235df93f9f1be19ca58a2d1109ed19c260f7cb71
Leighton Grant <oneal.grant@outlook.com>
2025-03-08 20:34:48 +0000

    Refactor BottomNavigation component to simplify footer styling and update copyright link

--- src/components/BottomNavigation.jsx
+++ src/components/BottomNavigation.jsx
@@ -2,16 +2,11 @@ import { Typography, Link } from '@mui/material'
 import Box from '@mui/material/Box'
 const FooterNavigation = () => {
 	return (
-		<Box
-			component={'footer'}
-			py={2}
-			mt={5}
-			sx={{ backgroundColor: 'transparent' }}
-		>
+		<Box component={'footer'} py={2} mt={2}>
 			<Typography variant='body2' align='center'>
 				{'Copyright © '}
-				<Link color='inherit' href='https://mui.com/'>
-					Longleigh Foundation Stakeholder Register
+				<Link color='inherit' href='https://longleigh.org'>
+					Longleigh Foundation
 				</Link>{' '}
 				{new Date().getFullYear()}
 				{'.'}
