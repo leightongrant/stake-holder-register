78ac4ee7ccd52f4b3ac37da7343f21b48689590a
Leighton Grant <oneal.grant@outlook.com>
2025-03-08 20:35:12 +0000

    Refactor ButtonAppBar component to integrate logo and streamline layout

--- src/components/ButtonAppBar.jsx
+++ src/components/ButtonAppBar.jsx
@@ -7,16 +7,16 @@ import {
 	MenuItem,
 	Avatar,
 	Menu,
-	Box,
 	Toolbar,
-	Typography,
 	IconButton,
+	Box,
 } from '@mui/material'
 import { Logout } from '@mui/icons-material'
 import { StorageSharp } from '@mui/icons-material'
 import { useState } from 'react'
 import Theme from './Theme'
 import { account } from '../lib/appwrite'
+import logo from '../../src/assets/longleigh-logo-no-sub-white.svg'
 
 export default function ButtonAppBar() {
 	const { user, setUser, setSession, setSessionId } = useStore()
@@ -47,86 +47,83 @@ export default function ButtonAppBar() {
 	}
 
 	return (
-		<Box sx={{ flexGrow: 1 }} component={'header'}>
-			<AppBar position='static'>
-				<Toolbar>
-					<Typography
-						color='inherit'
-						variant='h6'
-						component='div'
-						sx={{ flexGrow: 1 }}
-					>
-						Longleigh Foundation
-					</Typography>
-					<IconButton
-						onClick={handleClick}
-						size='small'
-						sx={{ ml: 2 }}
-						aria-controls={open ? 'account-menu' : undefined}
-						aria-haspopup='true'
-						aria-expanded={open ? 'true' : undefined}
-					>
-						<Avatar alt={user?.name} />
-					</IconButton>
+		<AppBar position='static'>
+			<Toolbar>
+				<Box
+					component='img'
+					src={logo}
+					alt='Longleigh Foundation'
+					width={80}
+					height={80}
+				/>
+				<IconButton
+					onClick={handleClick}
+					size='small'
+					sx={{ marginLeft: 'auto' }}
+					aria-controls={open ? 'account-menu' : undefined}
+					aria-haspopup='true'
+					aria-expanded={open ? 'true' : undefined}
+				>
+					<Avatar alt={user?.name} />
+				</IconButton>
 
-					<Menu
-						anchorEl={anchorEl}
-						id='account-menu'
-						open={open}
-						onClose={handleClose}
-						onClick={handleClose}
-						slotProps={{
-							paper: {
-								elevation: 0,
-								sx: {
-									overflow: 'visible',
-									filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-									mt: 1.5,
-									'& .MuiAvatar-root': {
-										width: 32,
-										height: 32,
-										ml: -0.5,
-										mr: 1,
-									},
-									'&::before': {
-										content: '""',
-										display: 'block',
-										position: 'absolute',
-										top: 0,
-										right: 14,
-										width: 10,
-										height: 10,
-										bgcolor: 'background.paper',
-										transform: 'translateY(-50%) rotate(45deg)',
-										zIndex: 0,
-									},
+				<Menu
+					anchorEl={anchorEl}
+					id='account-menu'
+					open={open}
+					onClose={handleClose}
+					onClick={handleClose}
+					slotProps={{
+						paper: {
+							elevation: 0,
+							sx: {
+								overflow: 'visible',
+								filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+								mt: 1.5,
+								'& .MuiAvatar-root': {
+									width: 32,
+									height: 32,
+									ml: -0.5,
+									mr: 1,
+								},
+								'&::before': {
+									content: '""',
+									display: 'block',
+									position: 'absolute',
+									top: 0,
+									right: 14,
+									width: 10,
+									height: 10,
+									bgcolor: 'background.paper',
+									transform: 'translateY(-50%) rotate(45deg)',
+									zIndex: 0,
 								},
 							},
-						}}
-						transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-						anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-					>
-						{user?.labels.includes('admin') && (
-							<MenuItem onClick={handleNavigate}>
-								<ListItemIcon>
-									<StorageSharp fontSize='small' />
-								</ListItemIcon>
-								Admin
-							</MenuItem>
-						)}
-						<MenuItem onClick={handleLogout}>
+						},
+					}}
+					transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+					anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+				>
+					{user?.labels.includes('admin') && (
+						<MenuItem onClick={handleNavigate}>
 							<ListItemIcon>
-								<Logout fontSize='small' />
+								<StorageSharp fontSize='small' />
 							</ListItemIcon>
-							Logout
+							Admin
 						</MenuItem>
-						<Divider />
-						<MenuItem>
-							<Theme />
-						</MenuItem>
-					</Menu>
-				</Toolbar>
-			</AppBar>
-		</Box>
+					)}
+					<MenuItem onClick={handleLogout}>
+						<ListItemIcon>
+							<Logout fontSize='small' />
+						</ListItemIcon>
+						Logout
+					</MenuItem>
+					<Divider />
+					<MenuItem>
+						<Theme />
+					</MenuItem>
+				</Menu>
+			</Toolbar>
+		</AppBar>
 	)
 }
