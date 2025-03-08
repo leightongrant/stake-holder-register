15f20288af234574762cb99a856bc4a8b5c24b3f
Leighton Grant <oneal.grant@outlook.com>
2025-03-08 10:55:09 +0000

    Refactor MainLayout component to use styled Stack for layout and improve background styling

--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -1,15 +1,39 @@
 import ButtonAppBar from '@/components/ButtonAppBar'
 import FooterNavigation from '../BottomNavigation'
-import { Box } from '@mui/material'
 import { Outlet } from 'react-router'
+import styled from '@emotion/styled'
+import { Stack } from '@mui/system'
+
 const MainLayout = () => {
+	const MainContainer = styled(Stack)(({ theme }) => ({
+		height: 'calc((1 - var(--template-frame-height, 0)) * 100dvh)',
+		minHeight: '100%',
+		padding: theme.spacing(2),
+		[theme.breakpoints.up('sm')]: {
+			padding: theme.spacing(0),
+		},
+		'&::before': {
+			content: '""',
+			display: 'block',
+			position: 'absolute',
+			zIndex: -1,
+			inset: 0,
+			backgroundImage:
+				'radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))',
+			backgroundRepeat: 'no-repeat',
+			...theme.applyStyles('dark', {
+				backgroundImage:
+					'radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))',
+			}),
+		},
+	}))
 	return (
 		<>
-			<ButtonAppBar />
-			<Box component='main'>
+			<MainContainer>
+				<ButtonAppBar />
 				<Outlet />
-			</Box>
-			<FooterNavigation />
+				<FooterNavigation />
+			</MainContainer>
 		</>
 	)
 }
