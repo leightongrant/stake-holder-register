dee195b62931f8e4b4d7b53e69ce634e2611fc48
Leighton Grant <oneal.grant@outlook.com>
2025-03-08 10:56:15 +0000

    Refactor BottomNavigation component to adjust footer styling with margin and transparent background

--- src/components/BottomNavigation.jsx
+++ src/components/BottomNavigation.jsx
@@ -2,7 +2,12 @@ import { Typography, Link } from '@mui/material'
 import Box from '@mui/material/Box'
 const FooterNavigation = () => {
 	return (
-		<Box component={'footer'} py={2}>
+		<Box
+			component={'footer'}
+			py={2}
+			mt={5}
+			sx={{ backgroundColor: 'transparent' }}
+		>
 			<Typography variant='body2' align='center'>
 				{'Copyright © '}
 				<Link color='inherit' href='https://mui.com/'>
